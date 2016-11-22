782eb44644e57ebb15083410dec5bdac78ea60d3
Esther Siram <isusecalea@yahoo.com>
2016-11-22 17:06:51 -0600

    commit: implement signup

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthenticationController extends AbstractController {
+	@Autowired
+	private UserDao userDao;
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupForm() {
@@ -35,7 +39,41 @@ public class AuthenticationController extends AbstractController {
 		
 		// TODO - implement login
 		
-		return "redirect:blog/newpost";
+		//1) get parameters from request object
+		//2) validate parameters (username, passwords, verify)--there are methods to do this in the user model class
+		//3) if good, create a new user & store in a session
+		//4) you can access the current session with the code: Session thisSession = request.getSession();
+		//note: you will use the method in the abstract controller called SetUserInSession (see video ~ 21:52)
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String verify = request.getParameter("verify");
+		
+		if(!password.equals(verify)){			    	//if passwords don't match.
+			model.addAttribute("verify_error", "The passwords do not match.");
+			model.addAttribute("username", username);
+			return "signup";
+		}
+		else{
+			if(!User.isValidUsername(username)){        //if invalid username
+				model.addAttribute("username_error", "Invalid username.");
+				model.addAttribute("username", username);
+				return "signup";
+			}
+			else if(!User.isValidPassword(password)){	//if valid username but invalid password
+				model.addAttribute("password_error", "Invalid password.");
+				model.addAttribute("username", username);
+				return "signup";
+			}
+			else{ //if everything qualifies and verifies   Note: Alyssa has this outside of the ELSE statement on hierarchical par with STRING in line 48
+				User newUser =  new User(username, password);
+				userDao.save(newUser);
+				HttpSession thisSession = request.getSession();
+				this.setUserInSession(thisSession, newUser);
+				return "redirect:blog/newpost";
+			}
+		}
+		
 	}
 	
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
