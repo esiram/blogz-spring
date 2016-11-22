e01a42de4c2b7fa47e6a112dfe26c0b1cdcbaece
Esther Siram <isusecalea@yahoo.com>
2016-11-22 17:10:53 -0600

    commit: correction:implement signup not login

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -26,7 +26,40 @@ public class AuthenticationController extends AbstractController {
 		
 		// TODO - implement signup
 		
-		return "redirect:blog/newpost";
+			//1) get parameters from request object
+			//2) validate parameters (username, passwords, verify)--there are methods to do this in the user model class
+			//3) if good, create a new user & store in a session
+			//4) you can access the current session with the code: Session thisSession = request.getSession();
+			//note: you will use the method in the abstract controller called SetUserInSession (see video ~ 21:52)
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String verify = request.getParameter("verify");
+		
+		if(!password.equals(verify)){			    	//if passwords don't match.
+			model.addAttribute("verify_error", "The passwords do not match.");
+			model.addAttribute("username", username);
+			return "signup";
+		}
+		else{
+			if(!User.isValidUsername(username)){        //if invalid username
+				model.addAttribute("username_error", "Invalid username.");
+				model.addAttribute("username", username);
+				return "signup";
+			}
+			else if(!User.isValidPassword(password)){	//if valid username but invalid password
+				model.addAttribute("password_error", "Invalid password.");
+				model.addAttribute("username", username);
+				return "signup";
+			}
+			else{ //if everything qualifies and verifies   Note: Alyssa has this outside of the ELSE statement on hierarchical par with STRING in line 35
+				User newUser =  new User(username, password);
+				userDao.save(newUser);
+				HttpSession thisSession = request.getSession();
+				this.setUserInSession(thisSession, newUser);
+				return "redirect:blog/newpost";
+			}
+		}
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -82,4 +115,3 @@ public class AuthenticationController extends AbstractController {
 		return "redirect:/";
 	}
 }
-//testing for github commit 11-21-16
